a7ff2bdb2ba191329fe97b767074888bbe07a6db
hsprime85 <hsprime85@gmail.com>
2022-03-09 11:21:40 -0800

    updated git ignore, add login, register function

--- WeSharper-Frontend/Frontend/src/app/home/home.component.ts
+++ WeSharper-Frontend/Frontend/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from '../_services/account.service';
@@ -8,19 +9,25 @@ import { AccountService } from '../_services/account.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  model: any = { }
 
+export class HomeComponent implements OnInit {
+  model: any = {}
+  users: any;
   constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
+  
   login(){
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/feeds');
+    }, error => {
+      console.log(error);
     })
   }
   
+
+
   logout(){
     this.accountService.logout();
     this.router.navigateByUrl('/');
--- WeSharper-Frontend/Frontend/src/app/register/register.component.ts
+++ WeSharper-Frontend/Frontend/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerMode = false;
+  model: any = {}
+  //users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   ngOnInit(): void {
+    //this.getUsers();
   }
 
+
+  register(){
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
+  }
+  // getUsers(){
+  //   this.http.get('https://localhost:7133/api/User/Profile').subscribe(users => this.users = users );
+  // }
 }
