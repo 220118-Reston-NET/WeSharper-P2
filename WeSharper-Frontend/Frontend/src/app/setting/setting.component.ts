bb8be9093179064614e01b16057a8a3199d91da0
Binta F <45748934+NabintouSFofana@users.noreply.github.com>
2022-03-10 01:33:26 -0600

    update setting page

--- WeSharper-Frontend/Frontend/src/app/setting/setting.component.ts
+++ WeSharper-Frontend/Frontend/src/app/setting/setting.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
@@ -10,6 +11,7 @@ export class SettingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  
   }
 
 }
