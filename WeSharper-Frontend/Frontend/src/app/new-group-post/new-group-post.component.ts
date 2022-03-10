2fc18f5aad07b5327bb8da47662fb0836310dfea
JasmineTseng7 <97990706+JasmineTseng7@users.noreply.github.com>
2022-03-10 02:00:58 -0800

    upload image

--- WeSharper-Frontend/Frontend/src/app/new-group-post/new-group-post.component.ts
+++ WeSharper-Frontend/Frontend/src/app/new-group-post/new-group-post.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,9 +6,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new-group-post.component.html',
   styleUrls: ['./new-group-post.component.css']
 })
-export class NewGroupPostComponent implements OnInit {
 
-  constructor() { }
+
+
+export class NewGroupPostComponent /*implements OnInit*/ {
+  selectedFile: File | null = null;
+
+  constructor(private http: HttpClient) { }
+
+  onFileSelect(event: { target: { files: File[]; }; }) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+
+  onUpload() {
+    const fd = new FormData;
+    if (this.selectedFile !== null) {
+      fd.append('image', this.selectedFile, this.selectedFile?.name)
+    }
+    this.http.post('upload url here', fd)
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
 
   ngOnInit(): void {
   }
