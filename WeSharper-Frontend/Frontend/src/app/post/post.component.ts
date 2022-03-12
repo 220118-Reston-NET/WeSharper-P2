d1b5bc7cb2ae954c25c1926e0ec100905e1fd060
Binta F <45748934+NabintouSFofana@users.noreply.github.com>
2022-03-11 19:15:41 -0600

    post function

--- WeSharper-Frontend/Frontend/src/app/post/post.component.ts
+++ WeSharper-Frontend/Frontend/src/app/post/post.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../_models/post';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
 
-  constructor() { }
+export class PostComponent implements OnInit {
+  post:Post;
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-  }
+    this.accountService.getPost().subscribe(result => {
+      this.post = result;
+  })
 
+}
 }
